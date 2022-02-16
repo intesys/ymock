b89cc5878d41a7ef4ca4e6f17a67ea2aa3e6a302
Alessandro Di Tecco <alessandro.ditecco@intesys.it>
2022-02-16 13:05:47 +0100

    Updated index.tsx with theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { SetupWorkerApi } from "msw";
-
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start();
-}
+import { MantineProvider } from "@mantine/core";
 
 declare global {
   interface Window {
@@ -20,12 +15,9 @@ declare global {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);

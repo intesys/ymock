85588947e77a6df5b93aabf097c18f8d060bb611
Alessandro Di Tecco <alessandro.ditecco@intesys.it>
2022-02-16 18:52:30 +0100

    Removed globals from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { SetupWorkerApi } from "msw";
 import { MantineProvider } from "@mantine/core";
-
-declare global {
-  interface Window {
-    msw: {
-      worker: SetupWorkerApi;
-      rest: any;
-    };
-  }
-}
 
 ReactDOM.render(
   <React.StrictMode>

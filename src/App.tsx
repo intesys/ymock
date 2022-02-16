64d445e86ae3eed42545d14d17017f855ed6b1db
Alessandro Di Tecco <alessandro.ditecco@intesys.it>
2022-02-16 18:55:32 +0100

    Moved layout logic out of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,142 +1,21 @@
-import React, { useState } from "react";
-import { MOCK_PATH } from "./constants";
+/* ---------------------------------
+App
+--------------------------------- */
+
+import React from "react";
+import Layout from "./components/Layout";
 
 function App() {
-  const [response, setResponse] = useState<string>("");
-  const [input, setInput] = useState<string>("");
-  const [toggleUI, setToggleUI] = useState<boolean>(false);
+  const { msw } = window;
+  const { worker, rest, handlers } = msw ?? {};
 
-  async function handleRequest() {
-    return (await fetch(MOCK_PATH)).json();
-  }
-
-  function setRuntimeRequestHandler(
-    body: Record<string, unknown>,
-    path: string = MOCK_PATH,
-    method: string = "get"
-  ) {
-    const { worker, rest } = window.msw;
-
-    worker.use(
-      rest[method](path, (req: any, res: any, ctx: any) => {
-        return res.once(ctx.json(body));
-      })
-    );
-  }
-
-  function handleReset() {
-    setInput("");
-    setResponse("");
+  if (!worker || !rest || !handlers) {
+    return <div>Error</div>;
   }
 
   return (
     <div className="App">
-      <div
-        style={{
-          maxWidth: 480,
-          margin: "0 auto",
-          fontFamily: "'Helvetica', 'Arial', system-ui, sans-serif",
-        }}
-      >
-        <section
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            padding: ".5rem",
-          }}
-        >
-          <pre style={{ padding: "3rem 1rem" }}>
-            <code>{response ? response : null}</code>
-          </pre>
-
-          <button
-            type="button"
-            onClick={() => {
-              handleReset();
-
-              handleRequest().then((response) =>
-                setResponse(JSON.stringify(response))
-              );
-            }}
-          >
-            Perform request
-          </button>
-        </section>
-
-        <section
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            padding: ".5rem",
-          }}
-        >
-          <button
-            type="button"
-            onClick={() => {
-              setToggleUI((ui) => !ui);
-            }}
-          >
-            Set runtime request handler
-          </button>
-
-          {toggleUI && (
-            <form
-              action="#"
-              onSubmit={(e) => {
-                if (!input) {
-                  window.alert("Please provide a value.");
-                  return;
-                }
-
-                e.preventDefault();
-                setRuntimeRequestHandler({ title: input });
-                handleReset();
-                setToggleUI(false);
-              }}
-            >
-              <input
-                type="text"
-                placeholder={"insert runtime response override..."}
-                onChange={(event) => setInput(event.target.value)}
-                value={input}
-              />
-
-              <button type="submit">Submit</button>
-            </form>
-          )}
-        </section>
-
-        <section
-          style={{
-            display: "flex",
-            padding: ".5rem",
-          }}
-        >
-          <button
-            type="button"
-            onClick={() => {
-              handleReset();
-              window.msw?.worker?.start?.();
-            }}
-          >
-            Start worker
-          </button>
-
-          <button
-            type="button"
-            onClick={() => {
-              handleReset();
-              window.msw?.worker?.stop?.();
-            }}
-          >
-            Stop worker
-          </button>
-
-          <button type="button" onClick={handleReset}>
-            Reset response
-          </button>
-        </section>
-      </div>
+      <Layout worker={worker} rest={rest} handlers={handlers} />
     </div>
   );
 }

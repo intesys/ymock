77fcb83623dbbf0da8a10bbd83073f4f68c96842
Alessandro Di Tecco <alessandro.ditecco@intesys.it>
2022-02-17 10:09:12 +0100

    Moved setRuntimeRequestHandler to lib

---
+++ src/lib/index.ts
@@ -0,0 +1,23 @@
+/* ---------------------------------
+lib
+--------------------------------- */
+
+import { SetupWorkerApi } from "msw";
+
+export function setRuntimeRequestHandler(
+  worker: SetupWorkerApi,
+  rest: {
+    [x: string]: (
+      arg0: string,
+      arg1: (req: any, res: any, ctx: any) => any
+    ) => any;
+  }
+) {
+  return function (body: string, path: string, method: string = "get") {
+    worker.use(
+      rest[method](path, (req: any, res: any, ctx: any) => {
+        return res.once(ctx.json(JSON.parse(body)));
+      })
+    );
+  };
+}

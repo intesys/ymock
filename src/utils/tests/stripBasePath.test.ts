de7f1c80732915302605bbb7ed4bcf73154c828a
Alessandro Di Tecco <alessandro.ditecco@intesys.it>
2022-02-17 12:05:50 +0100

    Added new tests in stripBasePath.test.ts, stripProtocol.test.ts

--- src/utils/tests/stripBasePath.test.ts
+++ src/utils/tests/stripBasePath.test.ts
@@ -1,7 +1,25 @@
 const { stripBasePath } = require("../index");
 
-test("Tests stripBasePath utility", () => {
+test("Tests stripBasePath utility with https", () => {
   expect(stripBasePath("https://jsonplaceholder.typicode.com/todos/100")).toBe(
     "/todos/100"
   );
+});
+
+test("Tests stripBasePath utility with http", () => {
+  expect(stripBasePath("http://jsonplaceholder.typicode.com/todos/100")).toBe(
+    "/todos/100"
+  );
+});
+
+test("Tests stripBasePath utility with more path levels", () => {
+  expect(
+    stripBasePath(
+      "https://jsonplaceholder.typicode.com/x/y/z/todos/100?x=y&y=z"
+    )
+  ).toBe("/x/y/z/todos/100?x=y&y=z");
+});
+
+test("Tests stripBasePath utility with falsy value", () => {
+  expect(stripBasePath(undefined)).toBe("");
 });
--- src/utils/tests/stripProtocol.test.ts
+++ src/utils/tests/stripProtocol.test.ts
@@ -1,7 +1,23 @@
 const { stripProtocol } = require("../index");
 
-test("Tests stripProtocol utility", () => {
+test("Tests stripProtocol utility with https", () => {
   expect(stripProtocol("https://jsonplaceholder.typicode.com/todos/100")).toBe(
     "jsonplaceholder.typicode.com/todos/100"
   );
+});
+
+test("Tests stripProtocol utility with http", () => {
+  expect(stripProtocol("http://jsonplaceholder.typicode.com/todos/100")).toBe(
+    "jsonplaceholder.typicode.com/todos/100"
+  );
+});
+
+test("Tests stripProtocol utility with non-matching value", () => {
+  expect(stripProtocol("jsonplaceholder.typicode.com/todos/100")).toBe(
+    "jsonplaceholder.typicode.com/todos/100"
+  );
+});
+
+test("Tests stripProtocol utility with falsy value", () => {
+  expect(stripProtocol(undefined)).toBe("");
 });

dc4b0cc6eb2e8ca685a903fc303b31421e7ad0b1
Alessandro Di Tecco <alessandro.ditecco@intesys.it>
2022-02-18 13:00:32 +0100

    Enabled notification system

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 
 ReactDOM.render(
   <React.StrictMode>
     <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles>
-      <App />
+      <NotificationsProvider
+        position="top-right"
+        limit={3}
+        autoClose={4000}
+        zIndex={999}
+      >
+        <App />
+      </NotificationsProvider>
     </MantineProvider>
   </React.StrictMode>,
   document.getElementById("root")

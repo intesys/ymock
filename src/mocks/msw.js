ef6ef0a4a437166c44610cef12e32e5d2e08321a
Alessandro Di Tecco <alessandro.ditecco@intesys.it>
2022-02-18 11:52:35 +0100

    Updated mocks in msw.js

--- src/mocks/msw.js
+++ src/mocks/msw.js
@@ -9,19 +9,15 @@ export default {
         header: "GET https://jsonplaceholder.typicode.com/users",
         path: "https://jsonplaceholder.typicode.com/users",
         method: "GET",
-        callFrame:
-          "RestHandler@http://localhost:3000/static/js/bundle.js:22897:5",
       },
     },
     {
       shouldSkip: false,
       ctx: {},
       info: {
-        header: "GET https://jsonplaceholder.typicode.com/todos/100",
+        header: "POST https://jsonplaceholder.typicode.com/todos/100",
         path: "https://jsonplaceholder.typicode.com/todos/100",
-        method: "GET",
-        callFrame:
-          "RestHandler@http://localhost:3000/static/js/bundle.js:22897:5",
+        method: "POST",
       },
     },
     {
@@ -31,8 +27,24 @@ export default {
         header: "GET https://jsonplaceholder.typicode.com/posts/100/comments",
         path: "https://jsonplaceholder.typicode.com/posts/100/comments",
         method: "GET",
-        callFrame:
-          "RestHandler@http://localhost:3000/static/js/bundle.js:22897:5",
+      },
+    },
+    {
+      shouldSkip: false,
+      ctx: {},
+      info: {
+        header: "GET https://jsonplaceholder.typicode.com/z",
+        path: "https://jsonplaceholder.typicode.com/z",
+        method: "GET",
+      },
+    },
+    {
+      shouldSkip: false,
+      ctx: {},
+      info: {
+        header: "DELETE https://jsonplaceholder.typicode.com/a",
+        path: "https://jsonplaceholder.typicode.com/a",
+        method: "DELETE",
       },
     },
   ],

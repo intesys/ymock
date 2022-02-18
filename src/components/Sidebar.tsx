8bcd98935700f46e8d2e4c06f40200e5d63687e1
Alessandro Di Tecco <alessandro.ditecco@intesys.it>
2022-02-18 11:53:14 +0100

    Added sorting in Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,14 +10,15 @@ import {
   Button,
   Divider,
   Group,
+  NativeSelect,
   Navbar,
   NavbarProps,
   Text,
-  UnstyledButton,
+  Title,
   useMantineTheme,
 } from "@mantine/core";
 import { RestHandler } from "msw";
-import { MSWglobalExports } from "../types";
+import { HandlerSortKeysType, MSWglobalExports } from "../types";
 import { stripBasePath } from "../utils";
 
 type OwnProps = Omit<NavbarProps, "children"> &
@@ -33,6 +34,7 @@ export default function Sidebar({
   ...navbarProps
 }: PropsWithChildren<OwnProps>): ReactElement | null {
   const [currentItem, setCurrentItem] = useState<RestHandler>();
+  const [select, setSelect] = useState<HandlerSortKeysType>("Select an option");
   const theme = useMantineTheme();
 
   function handleItemClick(item: RestHandler) {
@@ -40,66 +42,137 @@ export default function Sidebar({
     onItemClick && onItemClick(item);
   }
 
+  function handleSort(sortKey: HandlerSortKeysType) {
+    return function (a: RestHandler, b: RestHandler): number {
+      if (sortKey === "Name") {
+        if (a.info.path < b.info.path) {
+          return -1;
+        }
+
+        if (a.info.path > b.info.path) {
+          return 1;
+        }
+      }
+
+      //
+      else if (sortKey === "Method") {
+        if (a.info.method < b.info.method) {
+          return -1;
+        }
+
+        if (a.info.method > b.info.method) {
+          return 1;
+        }
+      }
+
+      return 0;
+    };
+  }
+
   return (
     <Navbar {...navbarProps}>
-      <Navbar.Section grow mt={theme.spacing.xl}>
+      <Navbar.Section grow mt={theme.spacing.sm}>
+        <header>
+          <Title
+            order={6}
+            sx={(t) => ({
+              padding: t.spacing.md,
+              textTransform: "uppercase",
+              fontSize: "small",
+            })}
+          >
+            Mocked requests
+          </Title>
+
+          <Divider
+            sx={{
+              borderColor: "#2C2E33", // TODO color not in theme
+            }}
+          />
+        </header>
+
+        {handlers?.length ? (
+          <section>
+            <NativeSelect
+              data={
+                ["Select an option", "Name", "Method"] as HandlerSortKeysType[]
+              }
+              label={
+                <Text size={"xs"} mb={"6px"}>
+                  Order by…
+                </Text>
+              }
+              sx={(t) => ({
+                padding: t.spacing.md,
+                paddingBottom: t.spacing.lg,
+              })}
+              value={select}
+              onChange={(e) =>
+                setSelect(e.currentTarget.value as HandlerSortKeysType)
+              }
+            />
+          </section>
+        ) : null}
+
         {handlers?.length
-          ? handlers.map((handler: RestHandler, i: number, arr) => {
-              const isSelected =
-                handler?.info?.path === currentItem?.info?.path;
+          ? [...handlers]
+              .sort(handleSort(select))
+              .map((handler: RestHandler, i: number, arr) => {
+                const isSelected =
+                  handler?.info?.path === currentItem?.info?.path;
 
-              return (
-                <>
-                  <Divider
-                    sx={{
-                      borderColor: "#2C2E33", // TODO color not in theme
-                    }}
-                  />
-                  <Box
-                    key={i}
-                    onClick={() => handleItemClick(handler)}
-                    sx={(t) => ({
-                      backgroundColor: isSelected
-                        ? t.colorScheme === "dark"
-                          ? t.colors.blue[9]
-                          : t.colors.indigo[9]
-                        : "none",
-                      padding: t.spacing.md,
-                      cursor: "pointer",
-                      fontSize: t.fontSizes.sm,
-
-                      "&:hover": {
-                        backgroundColor: isSelected
-                          ? t.colorScheme === "dark"
-                            ? t.colors.blue[8]
-                            : t.colors.gray[1]
-                          : t.colorScheme === "dark"
-                          ? t.colors.dark[5]
-                          : t.colors.gray[1],
-                      },
-                    })}
-                  >
-                    <Group spacing="md" noWrap>
-                      <Badge color={"green"} component={"span"}>
-                        {handler.info?.method}
-                      </Badge>
-
-                      <Text size={"sm"}>
-                        {stripBasePath(handler.info?.path)}
-                      </Text>
-                    </Group>
-                  </Box>
-
-                  {i === arr.length - 1 && (
+                return (
+                  <>
                     <Divider
                       sx={{
                         borderColor: "#2C2E33", // TODO color not in theme
                       }}
                     />
-                  )}
-                </>
-              );
-            })
+                    <Box
+                      key={i}
+                      onClick={() => handleItemClick(handler)}
+                      sx={(t) => ({
+                        backgroundColor: isSelected
+                          ? t.colorScheme === "dark"
+                            ? t.colors.blue[9]
+                            : t.colors.indigo[9]
+                          : "none",
+                        padding: t.spacing.md,
+                        cursor: "pointer",
+                        fontSize: t.fontSizes.sm,
+
+                        "&:hover": {
+                          backgroundColor: isSelected
+                            ? t.colorScheme === "dark"
+                              ? t.colors.blue[8]
+                              : t.colors.gray[1]
+                            : t.colorScheme === "dark"
+                            ? t.colors.dark[5]
+                            : t.colors.gray[1],
+                        },
+                      })}
+                    >
+                      <Group spacing="md" noWrap>
+                        <Badge color={"green"} component={"span"}>
+                          {handler.info?.method}
+                        </Badge>
+
+                        <Text size={"sm"}>
+                          {stripBasePath(handler.info?.path)}
+                        </Text>
+                      </Group>
+                    </Box>
+
+                    {i === arr.length - 1 && (
+                      <Divider
+                        sx={{
+                          borderColor: "#2C2E33", // TODO color not in theme
+                        }}
+                      />
+                    )}
+                  </>
+                );
+              })
           : null}
       </Navbar.Section>
 

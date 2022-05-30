9bcdead5af6b7992629088cf946bb7bb32345c61
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-05-30 11:21:49 +0200

    feature/11-create-a-ui-to-summon-the-app-from-a-host-app | Re-implemented launcher button with UI kit button, WIP

--- src/components/Launcher.tsx
+++ src/components/Launcher.tsx
@@ -6,6 +6,8 @@ import * as React from "react";
 import { PropsWithChildren } from "react";
 import { MSWglobalExports } from "../types";
 import { APP_BASE_PATH } from "../constants";
+import { Button, Menu, useMantineTheme } from "@mantine/core";
+import { ChevronDown, Package, SquareCheck } from "tabler-icons-react";
 
 type OwnProps = {
   msw: MSWglobalExports;
@@ -16,25 +18,58 @@ export default function Launcher({
 }: PropsWithChildren<OwnProps>): JSX.Element | null {
   if (!msw) return null;
 
+  const theme = useMantineTheme();
+
+  function handleNewWindowClick() {
+    const windowRef = window.open(
+      APP_BASE_PATH + "/",
+      "_blank",
+      "popup, right=100, top=100, width=1200, height=700"
+    );
+
+    if (windowRef) {
+      windowRef.msw = msw;
+    }
+  }
+
   return (
     <div>
-      <button
-        type={"button"}
-        style={{ marginTop: "2rem", width: "100%" }}
-        onClick={() => {
-          const windowRef = window.open(
-            APP_BASE_PATH + "/",
-            "_blank",
-            "popup, right=100, top=100, width=1200, height=700"
-          );
-
-          if (windowRef) {
-            windowRef.msw = msw;
-          }
-        }}
+      <Menu
+        control={
+          <Button
+            rightIcon={<ChevronDown size={18} />}
+            sx={{ paddingRight: 12 }}
+          >
+            🚀 yMock
+          </Button>
+        }
+        transition="pop-top-right"
+        placement="end"
+        size="lg"
       >
-        Open Admin UI
-      </button>
+        <Menu.Item
+          icon={<Package size={16} color={theme.colors.blue[6]} />}
+          onClick={handleNewWindowClick}
+          // rightSection={
+          //   <Text size="xs" transform="uppercase" weight={700} color="dimmed">
+          //     Ctrl + P
+          //   </Text>
+          // }
+        >
+          Launch in a new window
+        </Menu.Item>
+
+        <Menu.Item
+          icon={<SquareCheck size={16} color={theme.colors.pink[6]} />}
+          // rightSection={
+          //   <Text size="xs" transform="uppercase" weight={700} color="dimmed">
+          //     Ctrl + T
+          //   </Text>
+          // }
+        >
+          Launch Docked to bottom
+        </Menu.Item>
+      </Menu>
     </div>
   );
 }

e0f8d2d61d070ea292e2443030a302dd8384cf79
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-10 23:47:42 +0200

    Removed `Body` props from Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,10 +4,7 @@ Main
 
 import * as React from "react";
 import Body from "../components/Body";
-import { useSidebarContext } from "../components/Layout";
 
 export default function Home(): JSX.Element {
-  const { sidebarItem, onSubmit } = useSidebarContext();
-
-  return <Body sidebarItem={sidebarItem} onSubmit={onSubmit} />;
+  return <Body />;
 }

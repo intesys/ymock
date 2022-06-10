62d1324b6565c266ed8571021d587dfc1ed6f674
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-10 22:31:16 +0200

    Refactored Sidebar.tsx as generic component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,73 +3,31 @@ Nav
 --------------------------------- */
 
 import * as React from "react";
-import { PropsWithChildren, ReactElement, useState } from "react";
+import { PropsWithChildren, ReactElement } from "react";
 import {
-  Badge,
-  Box,
   Button,
   Divider,
   Group,
-  NativeSelect,
   Navbar,
   NavbarProps,
-  Text,
   Title,
   useMantineTheme,
 } from "@mantine/core";
-import { RestHandler } from "msw";
-import { HandlerSortKeysType, MSWglobalExports } from "../types";
-import { stripBasePath } from "../utils";
+import { useWorkerContext } from "../hooks";
 
-type OwnProps = Omit<NavbarProps, "children"> &
-  Pick<MSWglobalExports, "handlers" | "worker"> & {
-    onItemClick: (arg0: RestHandler) => void;
-    mainSectionTitle: string;
-  };
+export type SidebarProps = Omit<NavbarProps, "children"> & {
+  title: string;
+  workerControl?: boolean;
+};
 
 export default function Sidebar({
-  handlers,
-  worker,
-  onItemClick,
   children,
-  mainSectionTitle,
+  title,
+  workerControl = true,
   ...navbarProps
-}: PropsWithChildren<OwnProps>): ReactElement | null {
-  const [currentItem, setCurrentItem] = useState<RestHandler>();
-  const [select, setSelect] = useState<HandlerSortKeysType>("Select an option");
+}: PropsWithChildren<SidebarProps>): ReactElement | null {
+  const { worker } = useWorkerContext();
   const theme = useMantineTheme();
-
-  function handleItemClick(item: RestHandler) {
-    setCurrentItem(item);
-    onItemClick && onItemClick(item);
-  }
-
-  function handleSort(sortKey: HandlerSortKeysType) {
-    return function (a: RestHandler, b: RestHandler): number {
-      if (sortKey === "Name") {
-        if (a.info.path < b.info.path) {
-          return -1;
-        }
-
-        if (a.info.path > b.info.path) {
-          return 1;
-        }
-      }
-
-      //
-      else if (sortKey === "Method") {
-        if (a.info.method < b.info.method) {
-          return -1;
-        }
-
-        if (a.info.method > b.info.method) {
-          return 1;
-        }
-      }
-
-      return 0;
-    };
-  }
 
   return (
     <Navbar {...navbarProps}>
@@ -83,7 +41,7 @@ export default function Sidebar({
               fontSize: "small",
             })}
           >
-            {mainSectionTitle}
+            {title}
           </Title>
 
           <Divider
@@ -93,150 +51,72 @@ export default function Sidebar({
           />
         </header>
 
-        {handlers?.length ? (
-          <section>
-            <NativeSelect
-              data={
-                ["Select an option", "Name", "Method"] as HandlerSortKeysType[]
-              }
-              label={
-                <Text size={"xs"} mb={"6px"}>
-                  Order by…
-                </Text>
-              }
+        {/* Route-specific sidebars */}
+        {children}
+      </Navbar.Section>
+
+      {workerControl ? (
+        <>
+          <Divider
+            sx={(t) => ({
+              borderColor: t.colors.dark[5],
+            })}
+          />
+
+          <Navbar.Section>
+            <Title
+              order={6}
+              sx={(t) => ({
+                textTransform: "uppercase",
+                fontSize: "small",
+                padding: t.spacing.md,
+              })}
+            >
+              Worker control
+            </Title>
+
+            <Divider
+              sx={(t) => ({
+                borderColor: t.colors.dark[5],
+              })}
+            />
+
+            <Group
+              position={"left"}
+              spacing={"sm"}
               sx={(t) => ({
                 padding: t.spacing.md,
-                paddingBottom: t.spacing.lg,
+                paddingBottom: 32,
               })}
-              value={select}
-              onChange={(e) =>
-                setSelect(e.currentTarget.value as HandlerSortKeysType)
-              }
-            />
-          </section>
-        ) : null}
+            >
+              {/* TODO alternate based on MSW status (get it via global object?) */}
+              <Button
+                size="xs"
+                variant="outline"
+                compact
+                uppercase
+                onClick={() => {
+                  worker?.start?.();
+                }}
+              >
+                Start MSW
+              </Button>
 
-        {handlers?.length
-          ? [...handlers]
-              .sort(handleSort(select))
-              .map((handler: RestHandler, i: number, arr) => {
-                const isSelected =
-                  handler?.info?.path === currentItem?.info?.path;
-
-                return (
-                  <React.Fragment key={i}>
-                    <Divider
-                      sx={(t) => ({
-                        borderColor: t.colors.dark[5],
-                      })}
-                    />
-                    <Box
-                      onClick={() => handleItemClick(handler)}
-                      sx={(t) => ({
-                        backgroundColor: isSelected
-                          ? t.colorScheme === "dark"
-                            ? t.colors.dark[9]
-                            : t.colors.indigo[9]
-                          : "none",
-                        padding: t.spacing.md,
-                        cursor: "pointer",
-                        fontSize: t.fontSizes.sm,
-
-                        "&:hover": {
-                          backgroundColor: isSelected
-                            ? t.colorScheme === "dark"
-                              ? t.colors.dark[8]
-                              : t.colors.gray[1]
-                            : t.colorScheme === "dark"
-                            ? t.colors.dark[5]
-                            : t.colors.gray[1],
-                        },
-                      })}
-                    >
-                      <Group spacing="md" noWrap>
-                        <Badge color={"teal"} component={"span"}>
-                          {handler.info?.method}
-                        </Badge>
-
-                        <Text size={"sm"}>
-                          {stripBasePath(handler.info?.path)}
-                        </Text>
-                      </Group>
-                    </Box>
-
-                    {i === arr.length - 1 && (
-                      <Divider
-                        sx={(t) => ({
-                          borderColor: t.colors.dark[5],
-                        })}
-                      />
-                    )}
-                  </React.Fragment>
-                );
-              })
-          : null}
-      </Navbar.Section>
-
-      {children && <Navbar.Section>{children}</Navbar.Section>}
-
-      <Divider
-        sx={(t) => ({
-          borderColor: t.colors.dark[5],
-        })}
-      />
-
-      <Navbar.Section>
-        <Title
-          order={6}
-          sx={(t) => ({
-            textTransform: "uppercase",
-            fontSize: "small",
-            padding: t.spacing.md,
-          })}
-        >
-          Worker control
-        </Title>
-
-        <Divider
-          sx={(t) => ({
-            borderColor: t.colors.dark[5],
-          })}
-        />
-
-        <Group
-          position={"left"}
-          spacing={"sm"}
-          sx={(t) => ({
-            padding: t.spacing.md,
-            paddingBottom: 32,
-          })}
-        >
-          {/* TODO alternate based on MSW status (get it via global object?) */}
-          <Button
-            size="xs"
-            variant="outline"
-            compact
-            uppercase
-            onClick={() => {
-              worker?.start?.();
-            }}
-          >
-            Start MSW
-          </Button>
-
-          <Button
-            size="xs"
-            variant="outline"
-            compact
-            uppercase
-            onClick={() => {
-              worker?.stop?.();
-            }}
-          >
-            Stop MSW
-          </Button>
-        </Group>
-      </Navbar.Section>
+              <Button
+                size="xs"
+                variant="outline"
+                compact
+                uppercase
+                onClick={() => {
+                  worker?.stop?.();
+                }}
+              >
+                Stop MSW
+              </Button>
+            </Group>
+          </Navbar.Section>
+        </>
+      ) : null}
     </Navbar>
   );
 }

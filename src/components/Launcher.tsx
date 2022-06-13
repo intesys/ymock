99d5340673f2df548f1de4d8336ebc89228eb0fc
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-13 17:05:40 +0200

    feature/toggle_mocks_at_runtime | Added "close window" functionality to Launcher.tsx

--- src/components/Launcher.tsx
+++ src/components/Launcher.tsx
@@ -3,26 +3,58 @@ Launcher
 --------------------------------- */
 
 import * as React from "react";
+import { useRef, useState } from "react";
 import { MSWglobalExports } from "../types";
 import { APP_ROOT, ERROR__LAUNCHER_FATAL_ERROR } from "../constants";
-import { Alert, Button } from "@mantine/core";
+import { Alert, Button, useMantineTheme } from "@mantine/core";
+import { CSSObject } from "@mantine/styles/lib/tss";
 
 type OwnProps = {
   msw: MSWglobalExports;
 };
 
 export default function Launcher({ msw }: OwnProps): JSX.Element {
+  const [launched, setLaunched] = useState(false);
+  const theme = useMantineTheme();
+  const windowRef = useRef<Window | null>(null);
+
+  const buttonStyles: Partial<Record<string, CSSObject>> = {
+    root: {
+      padding: "0 20px",
+      height: 46,
+      boxShadow: "0 0 55px #00000066",
+      textTransform: "uppercase",
+      fontSize: "small",
+    },
+  };
+
   function handleNewWindowClick() {
     if (typeof window !== "undefined") {
-      const windowRef = window.open(
+      const w = window.open?.(
         APP_ROOT,
         "_blank",
         "popup, right=100, top=100, width=1100, height=700"
       );
 
-      if (windowRef) {
-        windowRef.msw = msw;
+      if (w) {
+        // load msw in global state
+        w.msw = msw;
+
+        // save a ref to the new window
+        windowRef.current = w;
+
+        setLaunched(true);
       }
+    }
+  }
+
+  function handleCloseWindowClick() {
+    if (windowRef.current) {
+      windowRef.current.close?.();
+
+      windowRef.current = null;
+
+      setLaunched(false);
     }
   }
 
@@ -49,23 +81,33 @@ export default function Launcher({ msw }: OwnProps): JSX.Element {
 
   return (
     <div className={"launcher"}>
-      <Button
-        radius="xl"
-        size="md"
-        styles={{
-          root: {
-            padding: "0 20px",
-            height: 46,
-            boxShadow: "0 0 55px #00000066",
-            textTransform: "uppercase",
-            fontSize: "small",
-          },
-        }}
-        onClick={handleNewWindowClick}
-      >
-        🚀
-        <span style={{ marginLeft: 8 }}>Launch yMock</span>
-      </Button>
+      {!launched ? (
+        <Button
+          radius="xl"
+          size="md"
+          styles={buttonStyles}
+          onClick={handleNewWindowClick}
+        >
+          🚀
+          <span style={{ marginLeft: 8 }}>Launch yMock</span>
+        </Button>
+      ) : (
+        <Button
+          radius="xl"
+          size="md"
+          styles={{
+            root: {
+              ...buttonStyles.root,
+              backgroundColor: theme.colors.gray[8],
+              "&:hover": { backgroundColor: theme.colors.gray[7] },
+            },
+          }}
+          onClick={handleCloseWindowClick}
+        >
+          🚫
+          <span style={{ marginLeft: 8 }}>Close yMock</span>
+        </Button>
+      )}
 
       <style jsx>{`
         .launcher {

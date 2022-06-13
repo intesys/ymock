9b51c9b67180861c569dc72059b21c82fc397fb0
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-13 17:40:05 +0200

    feature/toggle_mocks_at_runtime | Added `Indicator` to HomeSidebar.tsx

--- src/components/HomeSidebar.tsx
+++ src/components/HomeSidebar.tsx
@@ -5,7 +5,14 @@ HomeSidebar
 import * as React from "react";
 import { useContext, useState } from "react";
 import { useWorkerContext } from "../hooks";
-import { Badge, Divider, Group, NativeSelect, Text } from "@mantine/core";
+import {
+  Badge,
+  Divider,
+  Group,
+  Indicator,
+  NativeSelect,
+  Text,
+} from "@mantine/core";
 import { HandlerSortKeysType } from "../types";
 import { RestHandler } from "msw";
 import { stripBasePath } from "../utils";
@@ -83,15 +90,21 @@ export default function HomeSidebar(): JSX.Element {
                     onClick={() => setSidebarItem(handler)}
                     selected={isSelected}
                   >
-                    <Group spacing="md" noWrap>
-                      <Badge color={"teal"} component={"span"}>
-                        {handler.info?.method}
-                      </Badge>
+                    <Indicator
+                      position="middle-end"
+                      size={8}
+                      color={handler.shouldSkip ? "gray" : undefined}
+                    >
+                      <Group spacing="md" noWrap>
+                        <Badge color={"teal"} component={"span"}>
+                          {handler.info?.method}
+                        </Badge>
 
-                      <Text size={"sm"}>
-                        {stripBasePath(handler.info?.path)}
-                      </Text>
-                    </Group>
+                        <Text size={"sm"}>
+                          {stripBasePath(handler.info?.path)}
+                        </Text>
+                      </Group>
+                    </Indicator>
                   </SidebarItem>
                   {i === arr.length - 1 && (
                     <Divider

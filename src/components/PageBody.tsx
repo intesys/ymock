551ff14f12c091360dcf9d77b5811c49ebf41720
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-21 18:34:24 +0200

    Renamed Body.tsx => PageBody.tsx, refactored

---
+++ src/components/PageBody.tsx
@@ -0,0 +1,27 @@
+/* ---------------------------------
+Body
+--------------------------------- */
+
+import * as React from "react";
+import { PropsWithChildren, ReactElement, useContext } from "react";
+import { Box, Container } from "@mantine/core";
+import { SidebarContext } from "./Layout";
+import { RestHandler } from "msw";
+
+type OwnProps = {
+  blankSlateMessage?: string;
+};
+
+export default function PageBody({
+  blankSlateMessage,
+  children,
+}: PropsWithChildren<OwnProps>): ReactElement {
+  const { sidebarItem } = useContext(SidebarContext);
+  const { info } = (sidebarItem as unknown as RestHandler) ?? {};
+
+  return (
+    <Container sx={() => (!info ? { height: "100%" } : {})}>
+      <Box component={"main"}>{children}</Box>
+    </Container>
+  );
+}

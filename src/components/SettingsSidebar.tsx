2f4dfe7472c864dd03609eec07854cb8a75ad2e7
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-21 20:11:02 +0200

    Refactored SettingsSidebar.tsx to pass current sidebar item via router and not `sidebarItem` context

--- src/components/SettingsSidebar.tsx
+++ src/components/SettingsSidebar.tsx
@@ -3,23 +3,16 @@ HomeSidebar
 --------------------------------- */
 
 import * as React from "react";
-import { useContext } from "react";
-import { Divider, Group, useMantineTheme } from "@mantine/core";
-import { Link } from "react-router-dom";
+import { Divider } from "@mantine/core";
 import SidebarItem from "./SidebarItem";
-import { SidebarContext } from "./Layout";
 import { useStore } from "../state";
 
 export default function SettingsSidebar(): JSX.Element {
-  const { sidebarItem, setSidebarItem } = useContext(SidebarContext);
   const settings = useStore((s) => s.settings);
-
-  console.log(settings);
-  const theme = useMantineTheme();
 
   // TODO mock items
   return (
-    <React.Fragment>
+    <>
       {[
         {
           label: "Theme",
@@ -42,28 +35,12 @@ export default function SettingsSidebar(): JSX.Element {
           info: "Amet",
         },
       ].map((mockItem, i, arr) => (
-        <>
+        <React.Fragment key={i}>
           <SidebarItem
-            onClick={() => setSidebarItem(mockItem)}
-            selected={
-              (sidebarItem as { label: string })?.label === mockItem.label
-            }
-            key={i}
+            state={{ selected: mockItem }}
+            to={mockItem.label.toLowerCase() ?? ""}
           >
-            <Group spacing="md" noWrap>
-              <Link
-                style={{
-                  color: theme.colors.gray[1],
-                  textDecoration: "none",
-                  fontSize: "small",
-                  textTransform: "uppercase",
-                  display: "block",
-                }}
-                to={mockItem.label.toLowerCase() ?? ""}
-              >
-                {mockItem.label}
-              </Link>
-            </Group>
+            {mockItem.label}
           </SidebarItem>
 
           {i === arr.length - 1 && (
@@ -73,8 +50,8 @@ export default function SettingsSidebar(): JSX.Element {
               })}
             />
           )}
-        </>
+        </React.Fragment>
       ))}
-    </React.Fragment>
+    </>
   );
 }

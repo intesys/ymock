cfd987c6dd3ce986401792fb6570afc146e76c59
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-21 10:54:54 +0200

    Refactored Layout.tsx as the new App

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,16 +15,37 @@ import {
   useMantineTheme,
 } from "@mantine/core";
 import Sidebar from "./Sidebar";
-import { APP_HOME, APP_NAME } from "../constants";
+import { APP_HOME, APP_NAME, isStandaloneMode } from "../constants";
 import { Link, Outlet } from "react-router-dom";
 import {
   getRouteSpecificSidebar,
   RuntimeRequestHandlerType,
   setRuntimeRequestHandler,
 } from "../lib";
-import { useWorkerContext } from "../hooks";
 import { useLocation } from "react-router";
 import { Settings as SettingsIcon, ThreeDCubeSphere } from "tabler-icons-react";
+import Error from "../views/Error";
+import { MSWglobalExports } from "../types";
+import { rest } from "msw";
+
+let msw: MSWglobalExports;
+
+/*
+ * This app's use case is to manage a `msw`
+ * instance launched by a host app.
+ *
+ * @see: README.md:23 (Hosted mode)
+ * @see: README.md:30 (Standalone mode)
+ *
+ * */
+
+if (isStandaloneMode) {
+  const { rest } = require("msw");
+  const { worker } = require("../demo/mocks/browser");
+  const { handlers } = require("../demo/mocks/handlers");
+
+  msw = { rest, worker, handlers };
+}
 
 type SidebarItemAndSetter = {
   sidebarItem: Record<string, unknown>;
@@ -41,100 +62,113 @@ export type OutletContext = {
 // Sidebar context shared by all Consumers inside `Layout`
 export const SidebarContext = React.createContext<SidebarItemAndSetter>({});
 
+// Provides the worker's data & handlers to the whole app
+export const WorkerContext = React.createContext<MSWglobalExports>({
+  handlers: [],
+  rest: undefined,
+  worker: undefined,
+});
+
 export default function Layout(): JSX.Element {
+  const { worker, rest, handlers } = msw ?? window?.msw ?? {};
+  const fatalError = [worker, rest, handlers].some((truthy) => !truthy);
+
   const [showSidebar, setShowSidebar] = useState(false);
   const [sidebarItem, setSidebarItem] = useState<Record<string, unknown>>();
-  const { worker, rest } = useWorkerContext();
   const location = useLocation();
   const theme = useMantineTheme();
 
+  if (fatalError) return <Error />;
+
   return (
-    <SidebarContext.Provider value={{ sidebarItem, setSidebarItem }}>
-      <AppShell
-        navbarOffsetBreakpoint="sm"
-        fixed
-        header={
-          <Header
-            height={46}
-            sx={(t) => ({
-              color: t.colors.gray[1],
-              textDecoration: "none",
-            })}
-          >
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                height: "100%",
-                padding: "0 1rem",
-              }}
+    <WorkerContext.Provider value={{ worker, rest, handlers }}>
+      <SidebarContext.Provider value={{ sidebarItem, setSidebarItem }}>
+        <AppShell
+          navbarOffsetBreakpoint="sm"
+          fixed
+          header={
+            <Header
+              height={46}
+              sx={(t) => ({
+                color: t.colors.gray[1],
+                textDecoration: "none",
+              })}
             >
-              {/* TODO style tags */}
-              <Link
+              <div
                 style={{
-                  color: theme.colors.gray[1],
-                  textDecoration: "none",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  height: "100%",
+                  padding: "0 1rem",
                 }}
-                to={APP_HOME}
               >
-                <Group spacing={"xs"}>
-                  <ThreeDCubeSphere
-                    size={28}
-                    strokeWidth={1}
-                    color={"whitesmoke"}
-                  />
+                {/* TODO style tags */}
+                <Link
+                  style={{
+                    color: theme.colors.gray[1],
+                    textDecoration: "none",
+                  }}
+                  to={APP_HOME}
+                >
+                  <Group spacing={"xs"}>
+                    <ThreeDCubeSphere
+                      size={28}
+                      strokeWidth={1}
+                      color={"whitesmoke"}
+                    />
 
-                  <Text
-                    size={"sm"}
-                    weight={500}
-                    sx={() => ({ position: "relative", left: -6 })}
-                  >
-                    {APP_NAME}
-                  </Text>
+                    <Text
+                      size={"sm"}
+                      weight={500}
+                      sx={() => ({ position: "relative", left: -6 })}
+                    >
+                      {APP_NAME}
+                    </Text>
+                  </Group>
+                </Link>
+
+                <Group>
+                  <Button component={Link} to="settings" variant={"subtle"}>
+                    <SettingsIcon
+                      size={22}
+                      strokeWidth={1}
+                      color={"whitesmoke"}
+                    />
+                  </Button>
                 </Group>
-              </Link>
 
-              <Group>
-                <Button component={Link} to="settings" variant={"subtle"}>
-                  <SettingsIcon
-                    size={22}
-                    strokeWidth={1}
-                    color={"whitesmoke"}
+                <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                  <Burger
+                    opened={showSidebar}
+                    onClick={() => setShowSidebar((o) => !o)}
+                    size="sm"
+                    color={theme.colors.gray[6]}
+                    mr="xl"
                   />
-                </Button>
-              </Group>
-
-              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-                <Burger
-                  opened={showSidebar}
-                  onClick={() => setShowSidebar((o) => !o)}
-                  size="sm"
-                  color={theme.colors.gray[6]}
-                  mr="xl"
-                />
-              </MediaQuery>
-            </div>
-          </Header>
-        }
-        navbar={
-          <Sidebar
-            fixed
-            position={{ top: 0, left: 0 }}
-            hiddenBreakpoint="sm"
-            hidden={!showSidebar}
-            width={{ sm: 300, lg: 400 }}
-            {...getRouteSpecificSidebar(location)}
+                </MediaQuery>
+              </div>
+            </Header>
+          }
+          navbar={
+            <Sidebar
+              fixed
+              position={{ top: 0, left: 0 }}
+              hiddenBreakpoint="sm"
+              hidden={!showSidebar}
+              width={{ sm: 300, lg: 400 }}
+              {...getRouteSpecificSidebar(location)}
+            />
+          }
+        >
+          <Outlet
+            // https://reactrouter.com/docs/en/v6/hooks/use-outlet-context
+            context={{
+              onSubmit: setRuntimeRequestHandler(worker, rest),
+            }}
           />
-        }
-      >
-        <Outlet
-          // https://reactrouter.com/docs/en/v6/hooks/use-outlet-context
-          context={{
-            onSubmit: setRuntimeRequestHandler(worker, rest),
-          }}
-        />
-      </AppShell>
-    </SidebarContext.Provider>
+        </AppShell>
+      </SidebarContext.Provider>
+    </WorkerContext.Provider>
   );
 }

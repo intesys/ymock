0259569ae7770e5a1cf8a355138ab3ca5876f47b
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-21 18:34:49 +0200

    Refactored BlankSlate.tsx with fallback values

--- src/components/BlankSlate.tsx
+++ src/components/BlankSlate.tsx
@@ -4,16 +4,19 @@ BlankSlate
 
 import * as React from "react";
 import { ReactChild } from "react";
-import { Box, Center } from "@mantine/core";
+import { Box, Center, Text } from "@mantine/core";
+import { MESSAGE__SELECT_FROM_SIDEBAR } from "../constants";
 
 type OwnProps = {
-  children: ReactChild;
+  children?: ReactChild;
 };
 
 export default function BlankSlate({ children }: OwnProps): JSX.Element {
   return (
     <Center sx={() => ({ height: "100%", width: "100%" })}>
-      <Box>{children}</Box>
+      <Box>
+        {children ?? <Text size={"sm"}>{MESSAGE__SELECT_FROM_SIDEBAR}</Text>}
+      </Box>
     </Center>
   );
 }

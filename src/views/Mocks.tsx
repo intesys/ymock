0e0f134ce50ff99740942181d7e6ebca50e96ba6
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2022-06-28 18:34:06 +0200

    Wrapped Setting.tsx & Mocks.tsx in `PageBody`

--- src/views/Mocks.tsx
+++ src/views/Mocks.tsx
@@ -6,16 +6,19 @@ import * as React from "react";
 import { Outlet } from "react-router-dom";
 import { setRuntimeRequestHandler } from "../lib";
 import { useWorkerContext } from "../hooks";
+import PageBody from "../components/PageBody";
 
 export default function Mocks(): JSX.Element {
   const { worker, rest } = useWorkerContext();
 
   return (
-    <Outlet
-      // https://reactrouter.com/docs/en/v6/hooks/use-outlet-context
-      context={{
-        onSubmit: setRuntimeRequestHandler(worker, rest),
-      }}
-    />
+    <PageBody>
+      <Outlet
+        // https://reactrouter.com/docs/en/v6/hooks/use-outlet-context
+        context={{
+          onSubmit: setRuntimeRequestHandler(worker, rest),
+        }}
+      />
+    </PageBody>
   );
 }
--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -4,7 +4,12 @@ Settings
 
 import * as React from "react";
 import { Outlet } from "react-router-dom";
+import PageBody from "../components/PageBody";
 
 export default function Settings(): JSX.Element {
-  return <Outlet />;
+  return (
+    <PageBody>
+      <Outlet />
+    </PageBody>
+  );
 }

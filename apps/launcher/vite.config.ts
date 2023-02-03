55c1f94d7ae11aab526bbedc394b08f5b9d9fd93
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2023-02-03 17:11:42 +0100

    Updated vite.config.ts for Launcher component

--- apps/launcher/vite.config.ts
+++ apps/launcher/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      // https://vitejs.dev/guide/build.html#library-mode
+      entry: resolve(__dirname, "index.ts"),
+      name: "yMock Launcher",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});

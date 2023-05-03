693b75b3157fbc7e53f1b687c49d913905beb8ab
Alessandro Di Tecco <1767469+aditecco@users.noreply.github.com>
2023-05-03 20:17:13 +0200

    Added plugin to vite.config.ts in ymock-client

--- apps/ymock/vite.config.ts
+++ apps/ymock/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import watchAndRun from "vite-plugin-watch-and-run";
+import path from "path";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    // https://stackoverflow.com/questions/72618944/get-error-to-build-my-project-in-vite-top-level-await-is-not-available-in-the
+    target: "esnext",
+  },
+  plugins: [
+    // @ts-ignore
+    react(),
+
+    // @ts-ignore
+    watchAndRun([
+      {
+        name: "rebuild",
+        watchKind: ["add", "change", "unlink"],
+        watch: path.resolve("src/**/*.*"),
+        run: "npm run clean && npm run build && cross-env REPO_MODE=true npm run postinstall",
+      },
+    ]),
+  ],
+});
